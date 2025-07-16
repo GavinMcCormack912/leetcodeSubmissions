515006560f6b10cc228178a58f530e0d5011f187
Gavin McCormack <mccormackga@icloud.com>
2025-07-16 19:29:34 -0400

    File updated at 7/16/2025, 7:29:34 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,12 +1,6 @@
-# Last updated: 7/16/2025, 7:28:28 PM
+# Last updated: 7/16/2025, 7:29:34 PM
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums_dict = dict()
-        for num in nums:
-            nums_dict[num] = nums_dict.get(num, 0) + 1
-        
-        for val in nums_dict.values():
-            if val >= 2:
-                return True
-        return False
+        nums_set = set(nums)
+        return len(nums_set) < len(nums)
         
