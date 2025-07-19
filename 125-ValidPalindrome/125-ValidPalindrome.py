d9baf66b3553396e5d88c83d00a1ee3e35ec8945
Gavin McCormack <mccormackga@icloud.com>
2025-07-19 16:36:30 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,23 @@
+# Last updated: 7/19/2025, 4:36:30 PM
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = []
+        for char in s.lower():
+            if char.isalnum():
+                st.append(char)
+        n = len(st)
+        end = n-1
+        start = 0
+
+        while start < end and st[start] == st[end]:
+            start += 1
+            end -= 1
+        
+        if start == end:
+            if st[start] != st[end]:
+                return False
+            return True
+
+        if start < end:
+            return False
+        return True
