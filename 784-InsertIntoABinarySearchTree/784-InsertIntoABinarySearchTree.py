37a3972e2f1ff90e7e2255a13f78f31a7ec46b8e
Gavin McCormack <mccormackga@icloud.com>
2025-08-12 21:05:10 -0400

    Create 784-InsertIntoABinarySearchTree

---
+++ 784-InsertIntoABinarySearchTree/784-InsertIntoABinarySearchTree.py
@@ -0,0 +1,12 @@
+# Last updated: 8/12/2025, 9:05:10 PM
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        
+        return root
