d0c2f84570172df8659008552c592c180b75c0f3
Gavin McCormack <mccormackga@icloud.com>
2025-08-12 21:05:19 -0400

    Create 199-BinaryTreeRightSideView

---
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
@@ -0,0 +1,27 @@
+# Last updated: 8/12/2025, 9:05:19 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        queue = deque()
+        queue.append(root)
+        node_list = []
+        while len(queue) > 0:
+            node_list.append(queue[-1].val)
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+        return node_list
+
+        
